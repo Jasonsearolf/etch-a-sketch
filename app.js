88f3184469462455599d5912c1fdbf4764393719
Jason Searolf <jason@adatier.com>
2025-03-20 00:42:25 +0100

    project functional done

--- app.js
+++ app.js
@@ -2,25 +2,50 @@
 const buttonContainer	=	document.querySelector("#buttonContainer");
 const container			=	document.querySelector("#container");
 const buttonElement		=	document.createElement("button");
+let	gridNumber			=	0;
+let containerWidth		=	0;
 
-buttonElement.textContent	=	"Generar Grids";
-const btnGridGenerator		= 	buttonContainer.appendChild(buttonElement);
+buttonElement.textContent	=	"Start GAME!";
+const btnStartGame			= 	buttonContainer.appendChild(buttonElement);
 
 const gridGenerator = () => {
-	for (let i = 0; i < (16 * 16); i++){
-		let divElement = document.createElement("div");
-		divElement.classList.add("grid-item");
+	for (let i = 0; i < (gridNumber * gridNumber) && i < 100*100; i++){
+		let gridWrapper	= document.createElement("div");
+		let gridItem	= document.createElement("div");
+		let currentOpacity = 1;
 
-		divElement.addEventListener("mouseover", () => {
-			divElement.classList.add("grid-item-active");
-		});
-
-		container.appendChild(divElement);
+		gridWrapper.classList.add("grid-wrapper");
+		gridItem.classList.add("grid-item");
+		
+		gridItem.addEventListener("mouseover", () => {
+			if (currentOpacity > 0.05){
+				gridItem.dataset.opacity = currentOpacity.toFixed(1);
+				gridItem.style.opacity = currentOpacity;
+				currentOpacity -= 0.1;
+			}
+			if (!gridItem.style.backgroundColor){
+				let randomColor = `rgb(
+					${Math.floor(Math.random()*256)},
+					${Math.floor(Math.random()*256)},
+					${Math.floor(Math.random()*256)})`
+					/* divElement.classList.add("grid-item-active"); */
+					gridItem.style.backgroundColor = randomColor;
+				}
+			});
+			
+		gridWrapper.appendChild(gridItem);
+		container.appendChild(gridWrapper);
 	}
 }
  
-btnGridGenerator.addEventListener("click", () => {
+btnStartGame.addEventListener("click", () => {
 	container.innerHTML = "";
+	gridNumber = prompt("Enter the size of your grid max 100");
+	while (gridNumber > 100){
+		gridNumber = prompt("MAX 100");
+	}
+	containerWidth = (gridNumber * 20) + ((gridNumber - 1) * 2);
+	container.style.cssText = `width: ${containerWidth}px !important`;
 	gridGenerator();
 });
 
